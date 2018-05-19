76ea2e3b9cf108c8b4846ba1a729e162eec954d1
Rumen Novachkov <r.novachkov@gmail.com>
2018-05-19 17:57:17 +0300

    5.HTTP Context -> done.

---
+++ EduWebServer/EduWebServer/Server/HTTP/HttpContext.cs
@@ -0,0 +1,23 @@
+﻿namespace EduWebServer.Server.HTTP
+{
+    using System;
+
+    using Contracts;
+
+    public class HttpContext : IHttpContext
+    {
+        private readonly IHttpRequest request;
+
+        public HttpContext(string reqString)
+        {
+            if (reqString == null)
+            {
+                throw new ArgumentNullException($"Request string: {reqString} can't be null..");
+            }
+
+            this.request = new HttpRequest(reqString);
+        }
+
+        public IHttpRequest Request => this.request;
+    }
+}
