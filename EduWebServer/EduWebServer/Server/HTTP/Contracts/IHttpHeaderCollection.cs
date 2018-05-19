75a5167c61cb69d2eee90facdfe166dea2e1204a
BiserBorisov <butoff@abv.bg>
2018-05-19 12:35:09 +0300

    Issue #2 Header and HeaderCollection

---
+++ EduWebServer/EduWebServer/Server/HTTP/Contracts/IHttpHeaderCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduWebServer.Server.HTTP.Contracts
+{
+    public interface IHttpHeaderCollection
+    {
+        void AddHeader(HttpHeader header);
+
+        bool ContainsKey(string key);
+
+        HttpHeader GetHeader(string key);
+    }
+}
